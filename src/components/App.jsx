a99a1b4dd59529bfc3f706453546f85c1322ea6f
NCatalin93 <catalin.nedelcu93@gmail.com>
2024-05-02 13:03:30 +0300

    add number section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,33 +5,48 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contacts: [],
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      filter: '',
       name: '',
+      number: '',
     };
   }
 
   handleChange = event => {
-    this.setState({ name: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    const { name } = this.state;
+    const { name, number } = this.state;
     const id = nanoid();
-    const newContact = { id, name };
+    const newContact = { id, name, number };
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
       name: '',
+      number: '',
     }));
   };
 
+  handleFilterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
   render() {
-    const { name, contacts } = this.state;
+    const { name, contacts, number, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
     return (
       <div>
         <h1>Phonebook</h1>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Nume:</label>
+          <label htmlFor="name">Name:</label>
           <input
             type="text"
             name="name"
@@ -41,12 +56,34 @@ export class App extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
+          <label htmlFor="number">Number:</label>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
+        <div>
+          <label htmlFor="filter">Find contacts by name:</label>
+          <input
+            type="text"
+            id="filter"
+            name="filter"
+            value={filter}
+            onChange={this.handleFilterChange}
+          />
+        </div>
         <ul>
-          {contacts.map(contact => (
-            <li key={contact.id}>{contact.name}</li>
+          {filteredContacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
           ))}
         </ul>
       </div>
